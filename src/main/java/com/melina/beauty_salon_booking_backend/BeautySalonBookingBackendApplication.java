5717d8bc04fe89951d996634869abbf3907ae36c
Melinagamarra-2023 <gamarramelina.2002@gmail.com>
2026-05-11 21:39:33 -0300

    Initial project setup

---
+++ src/main/java/com/melina/beauty_salon_booking_backend/BeautySalonBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.melina.beauty_salon_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySalonBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melina/beauty_salon_booking_backend/BeautySalonBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melina.beauty_salon_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalonBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
